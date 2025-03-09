62f99d0924a94197fbcf54ca8a230dce380c41bc
loothyy <laythalsharrah12@gmail.com>
2025-03-09 22:22:24 +0200

    Update rayfield.lua

--- rayfield.lua
+++ rayfield.lua
@@ -1,14 +1,3 @@
---[[
-
-	Rayfield Interface Suite
-	by Sirius
-
-	shlex | Designing + Programming
-	iRay  | Programming
-	Max   | Programming
-
-]]
-
 if debugX then
 	warn('Initialising Rayfield')
 end
